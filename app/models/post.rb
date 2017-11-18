3538f51cad234fb6f6052ea54161f9382d51dcde
Mitchel <mitchel_jager@hotmail.com>
2017-11-18 21:46:43 +0100

    Force editted posts to be approved again

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ActiveRecord::Base
 
   attribute :user_name, :string
 
-  validates :title, presence: true, length: { minimum: 8 }, uniqueness: true
+  validates :title, presence: true, length: { minimum: 8 }
   validates :price, presence: true, numericality: true
   validates :user_id, presence: true
   validates_inclusion_of :categories, :in => ["buttons", "menus", "forms"]
