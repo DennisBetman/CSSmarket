8e9c6d0ce71cf9b42ab29955ce88b6731f02c20b
Mitchel <mitchel_jager@hotmail.com>
2017-12-23 18:00:11 +0100

    First try to get mailer working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      UserMailer.welcome(user).deliver_later
       session[:user_id] = user.id
       redirect_to root_path
     else
