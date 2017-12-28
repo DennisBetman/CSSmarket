b68b03454dc7114e0f8f8da09c9eb599daa670f6
Mitchel <mitchel_jager@hotmail.com>
2017-12-28 19:28:55 +0100

    empty test

--- test/controllers/seller_requests_controller_test.rb
+++ test/controllers/seller_requests_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class SellerRequestsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get seller_requests_create_url
-    assert_response :success
-  end
 
 end
