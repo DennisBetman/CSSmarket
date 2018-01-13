b2dfc3f793849ae798a9f9f85d8e5b6f1ba5d902
Dennis B <dennis@betman.nl>
2018-01-13 16:27:25 +0100

    New class names

--- db/migrate/20171230132315_rename_widthdrawls_to_withdrawals.rb
+++ db/migrate/20171230132315_rename_widthdrawls_to_withdrawals.rb
@@ -1,4 +1,4 @@
-class RenameWithdrawalsToWithdrawals < ActiveRecord::Migration[5.0]
+class RenameWidthdrawlsToWithdrawals < ActiveRecord::Migration[5.0]
   def change
      rename_table :widthdrawls, :withdrawals
    end
