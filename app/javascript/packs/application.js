75c950e284ea7e98aadaca5bbcfa56ca97969488
Mitcheljager <mitchel_jager@hotmail.com>
2018-09-29 01:01:44 +0200

    Revert "Add color-thief as vendor"
    This reverts commit dabd746cddbb7acdec8b52484189b2708ee1f068.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ Turbolinks.start()
 import "expose-loader?$!jquery"
 import "expose-loader?jQuery!jquery"
 
-import "../src/javascripts/vendor/color-thief"
 import "../src/javascripts/accordion"
 import "../src/javascripts/alert"
 import "../src/javascripts/charges"
